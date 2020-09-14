7895becd47e826d09b50fc70685e6b39097ecf14
Total-Anonimy <43591555+Total-Anonimy@users.noreply.github.com>
2020-09-14 11:03:01 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,32 +2,32 @@ const fs = require('fs');
 const Discord = require('discord.js');
 const { prefix, token } = require('./config.json');
 
-const client = new Discord.Client();
-client.commands = new Discord.Collection();
+const bot = new Discord.Client();
+bot.commands = new Discord.Collection();
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
-	client.commands.set(command.name, command);
+	bot.commands.set(command.name, command);
 }
 
 const cooldowns = new Discord.Collection();
 
-client.once('ready', () => {
+bot.once('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
 });
 
 
 
-client.on('message', message => {
+bot.on('message', message => {
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
 	const args = message.content.slice(prefix.length).trim().split(/ +/);
 	const commandName = args.shift().toLowerCase();
 
-	const command = client.commands.get(commandName)
-		|| client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+	const command = bot.commands.get(commandName)
+		|| bot.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
 
 	if (!command) return;
 
@@ -73,9 +73,9 @@ client.on('message', message => {
 	}
 });
 
-client.on('ready', () => {
-    client.user.setStatus('dnd')
-    client.user.setActivity("Meu prefixo é -, recomendo usar -help!", {type: "PLAYING"}).catch(console.error);
+bot.on('ready', () => {
+    bot.user.setStatus('dnd')
+    bot.user.setActivity("Meu prefixo é -, recomendo usar -help!", {type: "PLAYING"}).catch(console.error);
 });
 
-client.login(token);
+bot.login(token);
