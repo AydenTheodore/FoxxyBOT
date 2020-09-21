7e9549223abf63dc13a2d3177c33e57d78615d97
Total-Anonimy <43591555+Total-Anonimy@users.noreply.github.com>
2020-09-21 08:33:45 -0300

    Delete leave.js

--- commands/leave.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	name: 'leave',
-	description: 'Não gostou do server? ENTÃO SAIA AGORA!!',
-	execute(message, args) {
-		message.channel.send(`Ah é??, Não gostou do server, ${message.author}??? Então SAI DELE AGORA!! :face_with_symbols_over_mouth:`);
-	},
-};
