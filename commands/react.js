99609e89ae4eb338c50219dbb2c68291bbb62d5e
Total-Anonimy <43591555+Total-Anonimy@users.noreply.github.com>
2020-09-21 09:04:20 -0300

    Update react.js

--- commands/react.js
+++ commands/react.js
@@ -6,10 +6,10 @@ module.exports = {
 	description: "Reagirei da maneira que desejar.",
 	execute(message, args) {
 		if (args[0] === 'sad') {
-			message.react(`${sad}`);
+			message.react("<:sad:754838629536497797>");
 		} else
 		if (args[0] === 'hearty_eyes') {
-			message.react(`${heartyeyes}`);
+			message.react("<:hearty_eyes:754846478794489926>");
 		} else
 		message.react(args.join(''));
 	},
