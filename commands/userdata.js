9959b5c9014440189ee3981899aaddced92a429e
Total-Anonimy <43591555+Total-Anonimy@users.noreply.github.com>
2020-09-21 08:34:16 -0300

    Delete userdata.js

--- commands/userdata.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	name: 'userdata',
-	description: 'Mostra alguns dados sobre você!',
-	execute(message, args) {
-		message.channel.send(`Seu nome de usuário: ${message.author.username}\nSeu ID: ${message.author.id}\nAvatar:`);
-		message.channel.send(message.author.displayAvatarURL());
-	},
-};
