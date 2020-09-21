6f7041fbd52cf1d0c357b9aa0dc1e0186a86098a
Total-Anonimy <43591555+Total-Anonimy@users.noreply.github.com>
2020-09-21 08:34:03 -0300

    Delete say.js

--- commands/say.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	name: 'say',
-	description: 'Diga-me o que dizer.',
-	execute(message, args) {
-		message.delete();
-		message.channel.send(message.content.replace(`${prefix}say`, ''));
-	},
-};
